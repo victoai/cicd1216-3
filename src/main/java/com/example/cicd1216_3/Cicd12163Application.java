fb33bf2e3088ff9c24db614f1ebd9d585a206edc
victoai <victoai@naver.com>
2025-12-16 12:51:01 +0900

    dfdf

---
+++ src/main/java/com/example/cicd1216_3/Cicd12163Application.java
@@ -0,0 +1,13 @@
+package com.example.cicd1216_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicd12163Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cicd12163Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cicd1216_3/Cicd12163ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicd1216_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicd12163ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
